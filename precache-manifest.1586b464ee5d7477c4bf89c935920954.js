19dec5071f546eb4cd4f7a42b6da6e02fc2ba472
github username <github email address>
2019-09-23 23:56:32 -0400

    Updates

---
+++ precache-manifest.1586b464ee5d7477c4bf89c935920954.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6ba3a57a4218735f140db129a8e8f9",
+    "url": "/samip-portfolio/index.html"
+  },
+  {
+    "revision": "5f63d7211c0c8191c831",
+    "url": "/samip-portfolio/static/css/2.70c021c7.chunk.css"
+  },
+  {
+    "revision": "9f2144c5b2d8ceec08da",
+    "url": "/samip-portfolio/static/css/main.1f943fce.chunk.css"
+  },
+  {
+    "revision": "5f63d7211c0c8191c831",
+    "url": "/samip-portfolio/static/js/2.318545ac.chunk.js"
+  },
+  {
+    "revision": "9f2144c5b2d8ceec08da",
+    "url": "/samip-portfolio/static/js/main.4c8180cc.chunk.js"
+  },
+  {
+    "revision": "909497ba1732a23ca0c2",
+    "url": "/samip-portfolio/static/js/runtime-main.bfc0d54e.js"
+  },
+  {
+    "revision": "15678ffe04860272db359a247ec7fc08",
+    "url": "/samip-portfolio/static/media/CSS.3.15678ffe.svg"
+  },
+  {
+    "revision": "f2695a2895a6981182e922f249e4577d",
+    "url": "/samip-portfolio/static/media/flatiron-connect01.f2695a28.png"
+  },
+  {
+    "revision": "0b6e37178cb3bb8705e1e4fcc34c3c53",
+    "url": "/samip-portfolio/static/media/flatiton-connect02.0b6e3717.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/samip-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/samip-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/samip-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/samip-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/samip-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "766e2e45eea67c92965896e4fa559114",
+    "url": "/samip-portfolio/static/media/foodie01.766e2e45.png"
+  },
+  {
+    "revision": "6e11edf1d680688734751e483c5268c2",
+    "url": "/samip-portfolio/static/media/foodie02.6e11edf1.png"
+  },
+  {
+    "revision": "fc6b34b287e07e3891ced40285a89ad4",
+    "url": "/samip-portfolio/static/media/git.fc6b34b2.png"
+  },
+  {
+    "revision": "092bdf3e2423ce8d4f7a475862fee9a1",
+    "url": "/samip-portfolio/static/media/html.092bdf3e.svg"
+  },
+  {
+    "revision": "0f51e2810758848717b80d550ed6fb2e",
+    "url": "/samip-portfolio/static/media/insta01.0f51e281.png"
+  },
+  {
+    "revision": "0b0d7d9625a24b846a3d868c7488df4f",
+    "url": "/samip-portfolio/static/media/insta02.0b0d7d96.png"
+  },
+  {
+    "revision": "1f0f3724598e4166fad36a2c2173fbe3",
+    "url": "/samip-portfolio/static/media/js.1f0f3724.png"
+  },
+  {
+    "revision": "fd9941c7901d56d2fe08d0250ec8feaf",
+    "url": "/samip-portfolio/static/media/mypic.fd9941c7.png"
+  },
+  {
+    "revision": "6c9652ac8b1975ec4f40bc0c2fe724c0",
+    "url": "/samip-portfolio/static/media/postgresql.6c9652ac.png"
+  },
+  {
+    "revision": "4e687d20b069681f4ef6956fbaa7ba1e",
+    "url": "/samip-portfolio/static/media/rails-1-logo.4e687d20.png"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/samip-portfolio/static/media/react.04557c1b.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/samip-portfolio/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "cb75cc0083b5ccedbb4541df0ea89439",
+    "url": "/samip-portfolio/static/media/ruby.cb75cc00.png"
+  },
+  {
+    "revision": "afe0a719ae9829b69af56ef28e616cd4",
+    "url": "/samip-portfolio/static/media/tvproject01.afe0a719.png"
+  },
+  {
+    "revision": "75e6a3beea746eb059a50cd321a8b905",
+    "url": "/samip-portfolio/static/media/tvproject02.75e6a3be.png"
+  }
+]);
