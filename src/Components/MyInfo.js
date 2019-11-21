4d4dbfd1c09ade885c4a5145f8a5e15204a1941b
samip-sharma <samip.sharma963@gmail.com>
2019-11-21 14:08:33 -0500

    change resume link

--- src/Components/MyInfo.js
+++ src/Components/MyInfo.js
@@ -5,7 +5,7 @@ export default class MyInfo extends React.Component{
         return(
             <React.Fragment>
                  <div className="links-container">
-          <div><a href="https://drive.google.com/file/d/1H2WgOlll32f8vkwOIEG9KM7xSqg6EEBB/view?usp=sharing" > <i class="fa fa-file"></i>My Resume</a></div>
+          <div><a href="https://docs.google.com/document/d/1Sw1LeeR0J6Dn_KASVuJ0Sj5lJFUkHF1WbymxLdcktJQ/edit?usp=sharing" > <i class="fa fa-file"></i>My Resume</a></div>
           <div><a href="https://www.linkedin.com/in/samip-sharma/" ><i class="fa fa-linkedin"></i>Linkedin</a></div>
           <div><a href="https://medium.com/@samip.sharma963" > <i class="fa fa-medium"></i>Blogs</a> </div>
         </div>
