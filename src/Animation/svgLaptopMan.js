c43cd98ef568d17314449e30d898e9bd61bf9e5a
samip-sharma <samip.sharma963@gmail.com>
2019-12-07 21:47:16 -0500

    adds responsive

--- src/Animation/svgLaptopMan.js
+++ src/Animation/svgLaptopMan.js
@@ -42,7 +42,7 @@ export default class SvgLaptopMan extends React.Component{
 
     render(){
         return(
-            <React.Fragment>
+            <div className="svg-image-container">
                 <svg className="laptop-man" style={{"cursor":"pointer"}} onClick={this.handleSvgClick} width="100%" height="100%" viewBox="0 -100 1528 1147" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g id="undraw_back_home_nl5c 1">
                     <g id="wall-line">
@@ -182,7 +182,7 @@ export default class SvgLaptopMan extends React.Component{
                  </svg>
 
 
-            </React.Fragment>
+            </div>
         )
     }
 }
